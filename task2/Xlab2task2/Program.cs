03da0de29e073219bed1056f3ec2124750eacbbe
ilya <i.molotovshchikov@carx-tech.com>
2025-03-20 18:54:12 +0300

    Xlab-task2

---
+++ task2/Xlab2task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ilSmithXlab2
+{
+    internal class Program
+    {
+        class House
+        {
+            public string city;
+            public int housenumbers;
+
+            public void Print()
+            {
+                Console.WriteLine($"{city}, {housenumbers}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            House tower = new House();
+            tower.city = "Korolev";
+            tower.housenumbers = 100500;
+
+            tower.Print();
+        }
+    }
+}
