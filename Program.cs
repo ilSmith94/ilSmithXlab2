380aed32d0cbfcb157f4145d0dbbed0026c6ad1a
ilya <i.molotovshchikov@carx-tech.com>
2025-03-20 18:21:05 +0300

    XLAB-2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ilSmithXlab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b;
+            b = 4;
+        }
+    }
+}
